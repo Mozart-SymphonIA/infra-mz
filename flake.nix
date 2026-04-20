fc49300d966fae83b619ac7e0c3b085a6f16b1cb
klinsmann <carhuas.arenas@outlook.com>
2026-04-20 07:33:27 -0500

    flakes

--- flake.nix
+++ flake.nix
@@ -7,16 +7,23 @@
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+                inherit system;
+                config.allowUnfree = true;
+          };
       in
       {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            go_1_23
+            go_1_25
             gopls
             go-tools
+		gemini-cli
+		claude-code
           ];
           shellHook = ''
+export TERM=xterm-256color
+            export COLORTERM=truecolor
             echo "🎶 Mozart Symphony: Infra-mz Toolkit Environment"
             go version
           '';
